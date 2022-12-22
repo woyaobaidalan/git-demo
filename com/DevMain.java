c85eb4674fa65e3b5471259b080f29201721688c
liuhongge <1175015191@qq.com>
2022-12-22 11:56:31 +0800

    12.22 Dev_first_push

--- com/DevMain.java
+++ com/DevMain.java
@@ -3,9 +3,7 @@ public class DevMain {
     public static void main(String[] args) {
         System.out.println("版本分支dev : hello world");
 
-        System.out.println("现在进行冲突");
-
-        System.out.println("现在是dev版本上的内容");
+        System.out.println("进行分支的push");
     }
 
 }
